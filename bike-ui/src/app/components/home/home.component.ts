c8065000c70c4dad67f0efbd3d9c57331ed3c8ec
Renan Perez <renan.perez@whiteclarkegroup.com>
2019-11-26 14:50:48 -0500

    Updating home component

--- bike-ui/src/app/components/home/home.component.ts
+++ bike-ui/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BikeService } from '../../services/bike.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,27 @@ import { BikeService } from '../../services/bike.service';
   styleUrls: ['./home.component.styl']
 })
 export class HomeComponent implements OnInit {
+  models: string[] = [
+    'Globo MTB 29 Full Suspension',
+    'Globo Carbon Fiber Race Series',
+    'Globo Time Trial Blade'
+  ];
+  bikeForm: FormGroup;
+  validMessage: string = "";
 
   constructor(private bikeService: BikeService) { }
 
   ngOnInit() {
+    this.bikeForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      model: new FormControl('', Validators.required),
+      serialNumber: new FormControl('', Validators.required),
+      purchasePrice: new FormControl('', Validators.required),
+      purchaseDate: new FormControl('', Validators.required),
+      contact: new FormControl()
+    });
   }
 
 }
